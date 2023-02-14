40120da878795920f215fb27758bcd4336106e6e
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2023-02-14 11:02:47 -0800

    removed paddle velocity as no longer taken into accound

--- paddle.py
+++ paddle.py
@@ -15,12 +15,10 @@ class Paddle(pygame.sprite.Sprite):
         
         self.position = (pos_x, pos_y)
         self.position_old = self.position 
-        self.velocity = (0,0)
         self.image = pygame.Surface(PADDLE_DIMENSIONS)
         self.image.fill(BASE_COLOR)
         self.rect = self.image.get_rect()
         self.rect.center = self.position
-
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
@@ -35,14 +33,7 @@ class Paddle(pygame.sprite.Sprite):
         if(self.rect.colliderect(ball)):
             ball.paddleHit(self)
 
-    def updateVelocity(self):
-        vx = self.position[0] - self.position_old[0]
-        vy = self.position[1] - self.position_old[1]
-        self.velocity = (vx, vy)
-
-
     def update(self, ball):
-        self.updateVelocity()
         self.handleCollision(ball)
         self.rect.center = self.position
         self.handleBoundry(540)
@@ -53,12 +44,10 @@ class PlayerPaddle(Paddle):
 
     def update(self, mouse_pos, ball):
         (_, mouse_y) = mouse_pos
-        self.position_old = self.position
         self.position = (self.position[0], mouse_y)
         super().update(ball)
 
 class EnemyPaddle(Paddle):
-    
     def __init__(self, pos_x, pos_y):
         super().__init__(pos_x, pos_y)
         self.speed = BASE_SPEED
@@ -81,6 +70,5 @@ class EnemyPaddle(Paddle):
         if(y < ball_y):
             self.position = (x, y+self.speed)
             self.speed += step
-
-        self.position_old = (x, y)        
+        
         super().update(ball)
