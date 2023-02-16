e802fc7f878a810a3dff497b08c6a653a184b9ad
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2023-02-15 21:01:53 -0800

    added window scaling for score numbers

--- scorecard.py
+++ scorecard.py
@@ -8,10 +8,19 @@ class Score():
         (self.player, self.enemy) = START_SCORE
         self.font = pygame.font.Font("BitPap.ttf", 500)
 
+    def scaledText(self, text, color, dimensions):
+        (width, height) = dimensions
+        text_surface = self.font.render(str(text), True, color)
+        if text_surface.get_width() < width:
+            width = text_surface.get_width()
+        text_surface = pygame.transform.scale(text_surface, (width, height))
+        return text_surface
+
+
     def draw(self, screen, dimensions, color):
         (width, height) = dimensions
-        playerScore = self.font.render(str(self.player), True, color)
-        enemyScore = self.font.render(str(self.enemy), True, color)
+        playerScore = self.scaledText(self.player, color, (width // 3 , height * 2 // 3))
+        enemyScore = self.scaledText(self.enemy, color, (width // 3, height * 2 // 3))
         
         p_text = playerScore.get_rect()
         e_text = enemyScore.get_rect()
