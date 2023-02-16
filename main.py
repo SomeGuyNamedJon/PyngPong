164ddbabf60f782b44ed07e476070ec6a5f7519e
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2023-02-16 14:33:31 -0800

    added chdir to file path so main can be run outside of it's folder

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from paddle import PaddlePlayer, PaddleAI
 from ball import Ball
 from scorecard import Score
 import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 os.chdir("assets")
 
 pygame.init()
