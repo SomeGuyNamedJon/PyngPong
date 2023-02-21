fce52a9e3674872e333f217dbda00119b2091cc8
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2023-02-21 14:37:51 -0800

    gave buttons sounds

--- button.py
+++ button.py
@@ -1,9 +1,15 @@
+import os
 import pygame
 pygame.init()
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+os.chdir("assets")
 
 BASE_COLOR = (30, 30, 30)
 FONT_COLOR = (255, 255, 255)
 SELECTED_COLOR = (60, 60, 60)
+
+HOVER_SOUND = pygame.mixer.Sound("pongblipf5.wav")
+CLICK_SOUND = pygame.mixer.Sound("pongblipb3.wav")
 
 class Button(pygame.sprite.Sprite):
     def __init__(self,string,font,position,size,function): #Add given properties as parameters
@@ -18,6 +24,7 @@ class Button(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = self.position
         self.clicked = False
+        self.hover = False
 
         self.text = font.render(string, True, FONT_COLOR)
 
@@ -30,7 +37,7 @@ class Button(pygame.sprite.Sprite):
     def selected(self):
         mouse_pos = pygame.mouse.get_pos()
         return self.rect.collidepoint(mouse_pos)
-    
+
     def updatePosition(self, position):
         self.position = position
 
@@ -38,8 +45,14 @@ class Button(pygame.sprite.Sprite):
         self.rect.center = self.position
         if(self.selected()):
             self.image.fill(SELECTED_COLOR)
+            if not self.hover:
+                HOVER_SOUND.play()
+                self.hover = True
         else:
             self.image.fill(BASE_COLOR)
+            self.hover = False
+            self.clicked = False
 
     def click(self):
+        CLICK_SOUND.play()
         self.function()
