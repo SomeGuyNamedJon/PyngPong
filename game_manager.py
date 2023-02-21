af62f796e639559b8ac3e1995bdd6fefb37005ff
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2023-02-20 22:16:16 -0800

    created files for future classes

---
+++ game_manager.py
@@ -0,0 +1,6 @@
+import pygame
+pygame.init()
+
+class GameManager():
+    def __init__(self, settings, dimensions):
+        pass
---
+++ settings.py
@@ -0,0 +1,6 @@
+import pygame
+pygame.init()
+
+class Settings():
+    def __init__(self):
+        pass
